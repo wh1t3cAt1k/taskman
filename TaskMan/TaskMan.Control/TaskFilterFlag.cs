ebd42040038b2fda5afd9e451d7eef40c1085548
wh1t3cAt1k <pavel@kabir.ru>
2016-03-23 21:54:30 +0300

    Incorporate task index into FilterFlag

--- TaskMan/TaskMan.Control/TaskFilterFlag.cs
+++ TaskMan/TaskMan.Control/TaskFilterFlag.cs
@@ -12,7 +12,7 @@ namespace TaskMan.Control
 	/// </summary>
 	public class TaskFilterFlag<T>: Flag<T>, ITaskFilter
 	{
-		readonly Func<T, Task, bool> _filterPredicate;
+		readonly Func<T, Task, int, bool> _filterPredicate;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="TaskMan.Control.FilterFlag{T}"/> class.
@@ -20,10 +20,11 @@ namespace TaskMan.Control
 		/// <param name="filterPredicate">
 		/// The filter predicate function. Based on the current flag value (first
 		/// function argument), and a given <see cref="Task"/> object (second
-		/// function argument), must return a boolean value indicating whether 
+		/// function argument), and the current object's index (third function
+		/// argument), must return a boolean value indicating whether 
 		/// the task object must be included into the filtered sequence.
 		/// </param>
-		public TaskFilterFlag(string name, string alias, Func<T, Task, bool> filterPredicate)
+		public TaskFilterFlag(string name, string alias, Func<T, Task, int, bool> filterPredicate)
 			: base(name, alias)
 		{
 			if (filterPredicate == null)
@@ -34,9 +35,14 @@ namespace TaskMan.Control
 			_filterPredicate = filterPredicate;
 		}
 
+		public TaskFilterFlag(string name, string alias, Func<T, Task, bool> filterPredicate)
+			: this(name, alias, (flagValue, task, taskIndex) => filterPredicate(flagValue, task))
+		{ }
+
 		public IEnumerable<Task> Filter(IEnumerable<Task> taskSequence)
 		{
-			return taskSequence.Where(task => _filterPredicate(this.Value, task));
+			return taskSequence.Where(
+				(task, taskIndex) => _filterPredicate(this.Value, task, taskIndex));
 		}
 	}
 }
