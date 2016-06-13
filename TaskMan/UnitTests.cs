c2367d58834a00c75074bebb94df6e8320ce51ee
wh1t3cAt1k <pavel@kabir.ru>
2016-06-14 02:34:23 +0300

    Reorder usings

--- TaskMan/UnitTests.cs
+++ TaskMan/UnitTests.cs
@@ -23,11 +23,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 using NUnit.Framework;
 
 using TaskMan.Objects;
-using System.Text;
 
 namespace TaskMan
 {
